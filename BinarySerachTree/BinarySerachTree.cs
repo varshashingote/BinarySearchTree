4cce581d6daaaf87d01b69998c86f5f9c11bfbe0
varsha Gunjal <varshashingote01@gmail.com>
2023-02-13 23:12:47 +0530

    [varsha]Binary serach tree ceated

---
+++ BinarySerachTree/BinarySerachTree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerachTree
+{
+        public class BinarySearchTree<T> where T : IComparable
+        {
+            //UC1
+            //create a BST
+            int count = 0;
+            bool result = false;
+            Node<T> Root;
+            Node<T> Current;
+            public void InsertData(T data)
+            {
+                if (Root == null)
+                {
+                    this.Root = new Node<T>(data);
+                    this.Current = Root;
+                    count++;
+                    return;
+                }
+                if (this.Current.data.CompareTo(data) > 0)
+                {
+                    if (this.Current.leftNode == null)
+                    {
+                        this.Current.leftNode = new Node<T>(data);
+                        this.Current = Root;
+                        count++;
+                    }
+                    else
+                    {
+                        this.Current = this.Current.leftNode;
+                        InsertData(data);
+                    }
+                }
+                else
+                {
+                    if (this.Current.rightNode == null)
+                    {
+                        this.Current.rightNode = new Node<T>(data);
+                        this.Current = Root;
+                        count++;
+                    }
+                    else
+                    {
+                        this.Current = this.Current.rightNode;
+                        InsertData(data);
+                    }
+                }
+            }
+            public Node<T> GetRoot()
+            {
+                return this.Root;
+            }
+            public void Display(Node<T> node)
+            {
+                if (node != null)
+                {
+                    Display(node.leftNode);
+                    Console.WriteLine("Element in binary serach tree is: " + node.data);
+                    Display(node.rightNode);
+                }
+            }
+        }
+    }
+
+
+
---
+++ BinarySerachTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BinarySerachTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+            binarySearchTree.Display(binarySearchTree.GetRoot());
+            Console.ReadLine();
+        }
+    }
+}
